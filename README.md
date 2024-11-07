95ff18e51de897ca5f20e2c07a49b4c5788abb0c
bellacoats <89366459+bellacoats@users.noreply.github.com>
2024-11-07 12:40:58 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# bellacoats.github.io
+
+### Fall 2022 Web Repository 
+
+I'm Bella, a Senior majoring in Marketing and minoring in Creative Technology and Design! 
+
+This is my Github showcasing all of my in-class work as well as projects completed using HTML, CSS, and some JavaScript.
+
---
+++ final.js
@@ -0,0 +1,24 @@
+// slideshow pics
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// arrow controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.display = "block";
+}
+
+
